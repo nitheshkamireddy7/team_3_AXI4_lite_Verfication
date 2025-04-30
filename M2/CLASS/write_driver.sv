eeeac6823f2044fdd2dbf23d56b892d8badb52de
nitheshkamireddy7 <nitheshkamireddy2024@gmail.com>
2025-04-30 13:11:34 -0700

    Update write_driver.sv

--- M2/CLASS/write_driver.sv
+++ M2/CLASS/write_driver.sv
@@ -2,26 +2,37 @@ class write_driver;
   mailbox #(write_txn) wbox;
   virtual axi_if axi;
 
-  function new(mailbox #(write_txn),axi_if axi);
+ class write_driver;
+  mailbox #(write_txn) wbox;
+  virtual axi_if axi;
+
+  function new(mailbox #(write_txn) wbox, axi_if axi);
     this.wbox = wbox;
-    this.axi = axi;
+    this.axi  = axi;
   endfunction
 
   task run;
-    wbox.get(wtx);
-    axi.WDATA = wtx.WDATA;
-    axi.AWADDR = wtx.AWADDR;
-    axi.WSTRB  = wtx.WSTRB;
-    axi.AWVALID = 1;
-    axi.WVAILD = 1;
-    wait(axi.AWREADY && axi.WREADY);
-    axi.AWREADY = 0;
-    axi.WREADY  = 0;
-    wait(axi.BVALID);
-    axi.BREADY = 1;
-    @posedge(axi.ACLK);
-    axi.BREDAY =0;
-    //end of transaction
+    write_txn wtx;
+    forever begin
+      wbox.get(wtx);
+
+      
+      axi.AWADDR  <= wtx.AWADDR;
+      axi.WDATA   <= wtx.WDATA;
+      axi.WSTRB   <= wtx.WSTRB;
+      axi.AWVALID <= 1;
+      axi.WVALID  <= 1;
+
+      
+      wait (axi.AWREADY && axi.WREADY);
+
+      axi.AWVALID <= 0;
+      axi.WVALID  <= 0;
+      wait (axi.BVALID);
+      axi.BREADY <= 1;
+      @(posedge axi.ACLK);   
+      axi.BREADY <= 0;
+    end
   endtask
 endclass
     
