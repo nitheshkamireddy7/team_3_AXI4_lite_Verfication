e1b879f63949af1438d5736e71020fac5cd36851
nitheshkamireddy7 <nitheshkamireddy2024@gmail.com>
2025-04-30 11:55:42 -0700

    Update write_txn.sv

--- M2/CLASS/write_txn.sv
+++ M2/CLASS/write_txn.sv
@@ -3,9 +3,9 @@ class write_txn;
   rand bit [31:0] WDATA;
   rand bit [3 :0] WSTRB;
 
-  constraint AWADDR_range{AWADDR<4}};
+  constraint AWADDR_range{AWADDR<4};
   constraint WSTRB_valid {WSTRB inside{[15:0]};
-  constraint WDATA_random { WDATA inside {[0:$]};
+// constraint WDATA_random { WDATA inside {[0:$]};
 endclass
 
   
