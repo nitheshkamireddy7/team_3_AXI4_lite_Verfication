eb9ea9f08b453f1be1c00e4e89ca628588a86798
nitheshkamireddy7 <nitheshkamireddy2024@gmail.com>
2025-04-30 19:31:51 -0700

    Update write_monitor.sv

--- M2/CLASS/write_monitor.sv
+++ M2/CLASS/write_monitor.sv
@@ -1,7 +1,23 @@
 class write_monitor;
   virtual axi_if axi;
-  mailbox #(write_txn) wtx;
-  function new(mailbox #(write_txn) wtx, axi_if axi);
+  mailbox #(write_txn) wsbox;
+  function new(mailbox #(write_txn) wsbox, axi_if axi);
     this.axi = axi;
-    this.wtx = wtx;
+    this.wsbox = wsbox;
   endfunction
+
+  task run;
+    forever begin
+    wait (axi.AWVALID && axi.AWREADY);
+    wait (axi.WVALID && axi.WREADY);
+      @(posedge axi.ACLK);
+    write_txn wtx;
+    wtx = new();
+    wtx.AWADDR = axi.AWADDR;
+    wtx.WSTRB  = axi.WSTRB;
+    wtx.WDATA  = axi.WDATA;
+      wsbox.put(wtx);
+    end
+  endtask
+endclass
+
