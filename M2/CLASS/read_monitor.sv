b5f5fb9cc8acc3e83c232c122a738f10e131db80
nitheshkamireddy7 <nitheshkamireddy2024@gmail.com>
2025-05-01 02:35:13 -0700

    Update read_monitor.sv

--- M2/CLASS/read_monitor.sv
+++ M2/CLASS/read_monitor.sv
@@ -1,28 +1,32 @@
 class read_monitor;
   virtual axi_if axi;
   mailbox #(read_txn) rsbox;
-  function new(mailbox #(read_txn) rsbox,axi_if axi);
-    this.axi = axi;
+
+  // Constructor
+  function new(mailbox #(read_txn) rsbox, virtual axi_if axi);
+    this.axi   = axi;
     this.rsbox = rsbox;
   endfunction
 
+  // Run task
   task run;
-  read_txn rtx;
-  forever begin
-    wait(axi.ARVALID && axi.ARREADY);
-    bit [3:0] addr = axi.ARADDR;
+    read_txn rtx;
+    bit [3:0] addr;
+    forever begin
+      // Wait for read address handshake
+      wait (axi.ARVALID && axi.ARREADY);
+      addr = axi.ARADDR;  
 
-    
-    wait(axi.RVALID && axi.RREADY);
-    @(posedge axi.ACLK);
+      // Wait for read data handshake
+      wait (axi.RVALID && axi.RREADY);
+      rtx = new();
+      rtx.ARADDR = addr;
+      rtx.RDATA  = axi.RDATA;
+      rsbox.put(rtx);
 
-    
-    rtx = new();
-    rtx.ARADDR = addr;
-    rtx.RDATA  = axi.RDATA;
-    rsbox.put(rtx);
-  end
-endtask
-
-   
-    
+      // Wait for RVALID and RREADY to go low before next read
+      @(posedge axi.ACLK);
+      wait (!(axi.RVALID && axi.RREADY));
+    end
+  endtask
+endclass
