10d2c9faae61a9781762cafcd182273cc7a7e83d
nitheshkamireddy7 <nitheshkamireddy2024@gmail.com>
2025-04-30 20:14:20 -0700

    Update environment.sv

--- M2/CLASS/environment.sv
+++ M2/CLASS/environment.sv
@@ -1,7 +1,47 @@
 class environment;
   virtual axi_if axi;
+
+  mailbox #(read_txn) rbox;
+  mailbox #(read_txn) rsbox;
+  mailbox #(write_txn) wbox;
+  mailbox #(write_txn) wsbox;
+
+  write_generator wgen;
+  read_generator  rgen;
+  write_driver    wdrv;
+  read_driver     rdrv;
+  write_monitor   wmon;
+  read_monitor    rmon;
+  scoreboard      sb;
+  
   function new(axi_if axi);
     this.axi = axi;
+  
+    rbox = new();
+    rsbox = new();
+    wbox = new();
+    wsbox = new();
+    wgen = new(wbox);
+    rgen = new(rbox);
+    wdrv = new(wbox,axi);
+    rdrv = new(rbox,axi);
+    wmon = new(wsbox,axi);
+    rmon = new(rsbox,axi);
+    sb = new(wsbox,rsbox);
   endfunction
 
+  
+    task run;
+    fork
+      wgen.run();
+      rgen.run();
+      wdrv.run();
+      rdrv.run();
+      wmon.run();
+      rmon.run();
+      sb.run();
+    join_none
+  endtask
+endclass
+  
   
