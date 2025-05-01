67b8c4cd4316460376d9fe04379427e77d17b0cd
nitheshkamireddy7 <nitheshkamireddy2024@gmail.com>
2025-04-30 19:42:07 -0700

    Create read_monitor.sv

---
+++ M2/CLASS/read_monitor.sv
@@ -0,0 +1,23 @@
+class read_monitor;
+  virtual axi_if axi;
+  mailbox #(read_txn) rsbox;
+  function new(mailbox #(read_txn) rsbox,axi_if axi);
+    this.axi = axi;
+    this.rsbox = rsbox;
+  endfunction
+
+  task run;
+    read_txn rtx;
+    forever begin
+    wait( axi.ARVALID && axi.ARREADY);
+    wait( axi.RVALID && axi.RREADY);
+    @(posedge axi.ACLK);
+      rtx = new();
+      rtx.ARADDR = axi.ARADDR;
+      rtx.RDATA = axi.RDATA;
+      rsbox.put(rtx);
+    end
+  endtask
+endclass
+   
+    
