22f5162d1da89e77ab3eeb4b5a480594308513df
nitheshkamireddy7 <nitheshkamireddy2024@gmail.com>
2025-05-01 02:37:06 -0700

    Update write_monitor.sv

--- M2/CLASS/write_monitor.sv
+++ M2/CLASS/write_monitor.sv
@@ -1,23 +1,29 @@
 class write_monitor;
   virtual axi_if axi;
   mailbox #(write_txn) wsbox;
-  function new(mailbox #(write_txn) wsbox, axi_if axi);
-    this.axi = axi;
+
+
+  function new(mailbox #(write_txn) wsbox, virtual axi_if axi);
+    this.axi   = axi;
     this.wsbox = wsbox;
   endfunction
 
+  // Run task
   task run;
+    write_txn wtx; 
     forever begin
-    wait (axi.AWVALID && axi.AWREADY);
-    wait (axi.WVALID && axi.WREADY);
+      wait (axi.AWVALID && axi.AWREADY);
+      wait (axi.WVALID && axi.WREADY);
       @(posedge axi.ACLK);
-    write_txn wtx;
-    wtx = new();
-    wtx.AWADDR = axi.AWADDR;
-    wtx.WSTRB  = axi.WSTRB;
-    wtx.WDATA  = axi.WDATA;
+
+      wtx = new();  // create a fresh transaction
+      wtx.AWADDR = axi.AWADDR;
+      wtx.WSTRB  = axi.WSTRB;
+      wtx.WDATA  = axi.WDATA;
+      $display("MONITOR: Captured write to addr=%0d, wstrb=%b, data=%h", wtx.AWADDR, wtx.WSTRB, wtx.WDATA);
+
+
       wsbox.put(wtx);
     end
   endtask
 endclass
-
