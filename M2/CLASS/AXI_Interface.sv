bc64684c8a7bcd3c9ebc9f21a5f612ba695fdd7d
nitheshkamireddy7 <nitheshkamireddy2024@gmail.com>
2025-05-01 02:33:00 -0700

    Update AXI_Interface.sv

--- M2/CLASS/AXI_Interface.sv
+++ M2/CLASS/AXI_Interface.sv
@@ -1,12 +1,13 @@
+
 //Team 3 AXI 4 Lite interface defined with modports
 
-interface axi_if #(parameter ADDR_WIDTH = 4, DATA_WIDTH = 32) (input logic ACLK, input logic ARESETn);
-  logic [ADDR_WIDTH-1:0] AWADDR;
+interface axi_if (input logic ACLK, input logic ARESETn);
+  logic [3:0] AWADDR;
   logic                  AWVALID;
   logic                  AWREADY;
 
-  logic [DATA_WIDTH-1:0] WDATA;
-  logic [(DATA_WIDTH/8)-1:0] WSTRB;
+  logic [31:0] WDATA;
+  logic [3:0] WSTRB;
   logic                  WVALID;
   logic                  WREADY;
 
@@ -14,11 +15,11 @@ interface axi_if #(parameter ADDR_WIDTH = 4, DATA_WIDTH = 32) (input logic ACLK,
   //logic [1:0]            BRESP;
   logic                  BREADY;
 
-  logic [ADDR_WIDTH-1:0] ARADDR;
+  logic [3:0] ARADDR;
   logic                  ARVALID;
   logic                  ARREADY;
 
-  logic [DATA_WIDTH-1:0] RDATA;
+  logic [31:0] RDATA;
   logic                  RVALID;
   //logic [1:0]            RRESP;
   logic                  RREADY;
