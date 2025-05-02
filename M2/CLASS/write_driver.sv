890fe73993d5c280dad70654ea41d2d3240dcf71
nitheshkamireddy7 <nitheshkamireddy2024@gmail.com>
2025-05-01 17:36:17 -0700

    Update write_driver.sv

--- M2/CLASS/write_driver.sv
+++ M2/CLASS/write_driver.sv
@@ -2,38 +2,38 @@ class write_driver;
   mailbox #(write_txn) wbox;
   virtual axi_if axi;
 
-  // Constructor with correct virtual interface declaration
   function new(mailbox #(write_txn) wbox, virtual axi_if axi);
     this.wbox = wbox;
     this.axi  = axi;
   endfunction
 
-  // Run task
   task run;
-    write_txn wtx; 
+    write_txn wtx;
     forever begin
       wbox.get(wtx);
 
+      // Drive address and data
+      @(posedge axi.ACLK);
       axi.AWADDR  <= wtx.AWADDR;
       axi.WDATA   <= wtx.WDATA;
       axi.WSTRB   <= wtx.WSTRB;
       axi.AWVALID <= 1;
       axi.WVALID  <= 1;
-      $display("DRIVER: Sending write Addr=%0d, WSTRB=%b, WDATA=%h",
-          wtx.AWADDR, wtx.WSTRB, wtx.WDATA);
+      axi.BREADY  <= 0;
 
-      
+     
 
+      // Wait for both AWREADY and WREADY handshake
       wait (axi.AWREADY && axi.WREADY);
 
       
+      
 
+      // Wait for BVALID and assert BREADY
       wait (axi.BVALID);
+      @(posedge axi.ACLK);
       axi.BREADY <= 1;
 
-      @(posedge axi.ACLK);  // Wait for a clock cycle
-      axi.BREADY <= 0;
-      
       
     end
   endtask
