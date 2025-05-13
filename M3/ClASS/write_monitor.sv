59eab5da63668244cdbdcc27cae284477504442d
nitheshkamireddy7 <nitheshkamireddy2024@gmail.com>
2025-05-13 16:44:18 -0700

    Update write_monitor.sv

--- M3/ClASS/write_monitor.sv
+++ M3/ClASS/write_monitor.sv
@@ -1,11 +1,13 @@
 class write_monitor;
   virtual axi_if axi;
   mailbox #(write_txn) wsbox;
+  mailbox #(write_txn) wcov;
 
 
-  function new(mailbox #(write_txn) wsbox, virtual axi_if axi);
+  function new(mailbox #(write_txn) wsbox, virtual axi_if axi, mailbox #(write_txn) wcov);
     this.axi   = axi;
     this.wsbox = wsbox;
+    this.wcov = wcov;
   endfunction
 
   // Run task
@@ -24,6 +26,7 @@ class write_monitor;
 
 
       wsbox.put(wtx);
+      wcov.put(wtx);
     end
   endtask
 endclass
