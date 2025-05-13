3e030c176e13cd768baf940cb8aac43e2a057b89
nitheshkamireddy7 <nitheshkamireddy2024@gmail.com>
2025-05-13 16:54:29 -0700

    Update AXI_coverage.sv

--- M3/ClASS/AXI_coverage.sv
+++ M3/ClASS/AXI_coverage.sv
@@ -1,5 +1,53 @@
 class axi_coverage;
+  
+  read_txn rtx;
+  write_txn wtx;
 
   
+  covergroup cg_read;
+    coverpoint rtx.ARADDR {
+      bins addr_vals[] = {0, 4, 8, 12};
+    }
+
+    coverpoint rtx.RDATA {
+      bins data_bins[] = {[0:32'hFFFF]};
+    }
+
+    cross rtx.ARADDR, rtx.RDATA;
+  endgroup
+
+  
+  covergroup cg_write;
+    coverpoint wtx.AWADDR {
+      bins addr_vals[] = {0, 4, 8, 12};
+    }
+
+    coverpoint wtx.WDATA {
+      bins data_bins[] = {[0:32'hFFFF]};
+    }
+
+    coverpoint wtx.WSTRB {
+      bins strobe_vals[] = {[0:15]};
+    }
+
+    cross wtx.AWADDR, wtx.WDATA, wtx.WSTRB;
+  endgroup
+
+  
+  function new();
+    cg_read = new();
+    cg_write = new();
+  endfunction
+
+  
+  task sample_read(read_txn t);
+    rtx = t;
+    cg_read.sample();
+  endtask
+
+  task sample_write(write_txn t);
+    wtx = t;
+    cg_write.sample();
+  endtask
 
 endclass
