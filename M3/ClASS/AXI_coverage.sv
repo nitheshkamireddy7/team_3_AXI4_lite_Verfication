9a865e051f7271beb60b4402f36852ca4be0d55c
nitheshkamireddy7 <nitheshkamireddy2024@gmail.com>
2025-05-19 19:42:04 -0700

    cross coverage has been added

--- M3/ClASS/AXI_coverage.sv
+++ M3/ClASS/AXI_coverage.sv
@@ -1,45 +1,57 @@
 class axi_coverage;
-  
+  // Transactions for sampling
   read_txn rtx;
   write_txn wtx;
 
-  
+  // Covergroup for READ transactions
   covergroup cg_read;
     coverpoint rtx.ARADDR {
       bins addr_vals[] = {0, 4, 8, 12};
     }
 
     coverpoint rtx.RDATA {
-      bins data_bins[] = {[0:32'hFFFF]};
+      bins low_data   = {[0:15]};
+      bins mid_data   = {[16:255]};
+      bins high_data  = {[256:65535]};
     }
-
-    cross rtx.ARADDR, rtx.RDATA;
+    
+    
   endgroup
 
+  // Covergroup for WRITE transactions
+ covergroup cg_write;
+
+  // Address coverpoint with explicit bins
+  cp_awaddr : coverpoint wtx.AWADDR {
+    bins addr_vals[] = {0, 4, 8, 12};
+  }
+
+  cp_wdata : coverpoint wtx.WDATA {
+    bins low_data   = {[0:15]};
+    bins mid_data   = {[16:255]};
+    bins high_data  = {[256:65535]};
+  }
+
+  cp_wstrb : coverpoint wtx.WSTRB {
+    bins strobe_1bit  = {[1:1]};
+    bins strobe_2bit  = {[2:3]};
+    bins strobe_4bit  = {[4:15]};
+  }
+
   
-  covergroup cg_write;
-    coverpoint wtx.AWADDR {
-      bins addr_vals[] = {0, 4, 8, 12};
-    }
+  cross cp_awaddr, cp_wstrb;
 
-    coverpoint wtx.WDATA {
-      bins data_bins[] = {[0:32'hFFFF]};
-    }
+endgroup
 
-    coverpoint wtx.WSTRB {
-      bins strobe_vals[] = {[0:15]};
-    }
 
-    cross wtx.AWADDR, wtx.WDATA, wtx.WSTRB;
-  endgroup
 
-  
+  // Constructor
   function new();
     cg_read = new();
     cg_write = new();
   endfunction
 
-  
+  // Sample functions
   task sample_read(read_txn t);
     rtx = t;
     cg_read.sample();
